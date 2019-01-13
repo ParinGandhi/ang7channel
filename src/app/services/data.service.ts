1954c9f2dd15a52917854235c9e44a6aa25693e6
ParinGandhi <parin.a.gandhi@gmail.com>
2019-01-13 13:05:12 -0500

    Added window.location to data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -25,23 +25,42 @@ export class DataService {
   // easMediaDataList: EasMediaData[];
   // stndSiteList: StandardSite[];
   // stndRole: StandardRole;
-  getChannelListUrl: string = 'http://localhost:8080/eas-media-data';
-  getSiteIdListUrl: string = 'http://localhost:8080/reference-data?type=site';
-  getRoleIdListUrl: string = 'http://localhost:8080/reference-data?type=role';
-  addChannelUrl: string = 'http://localhost:8080/eas-media-data';
-  addSiteUrl: string = 'http://localhost:8080/standard-site';
-  addRoleUrl: string = 'http://localhost:8080/standard-role';
-  historyDataUrl: string = 'http://localhost:8080/eas-media-data-history';
-  archiveChannelsUrl: string = 'http://localhost:8080/eas-media-data/archive';
-  updateSiteUrl: string = 'http://localhost:8080/standard-site';
-  updateRoleUrl: string = 'http://localhost:8080/standard-role';
-  updateChannelUrl: string = 'http://localhost:8080/eas-media-data';
-  getClassificationUrl: string = 'http://localhost:8080/reference-data?type=clsf';
-  addClassificationUrl: string = 'http://localhost:8080/standard-classification';
-  updateClassificationUrl: string = 'http://localhost:8080/standard-classification';
-  dashboardInfoUrl: string = 'http://localhost:8080/dashboard-info';
-  getChannelData: string = 'http://localhost:8080/eas-media-data-activity?channelName=';
-  urlByChannelName: string = 'http://localhost:8080//fetch-audio'
+  location: any = window.location;
+  baseUrl: string = this.getUrlBase();
+  // getChannelListUrl: string = 'http://localhost:8080/eas-media-data';
+  // getSiteIdListUrl: string = 'http://localhost:8080/reference-data?type=site';
+  // getRoleIdListUrl: string = 'http://localhost:8080/reference-data?type=role';
+  // addChannelUrl: string = 'http://localhost:8080/eas-media-data';
+  // addSiteUrl: string = 'http://localhost:8080/standard-site';
+  // addRoleUrl: string = 'http://localhost:8080/standard-role';
+  // historyDataUrl: string = 'http://localhost:8080/eas-media-data-history';
+  // archiveChannelsUrl: string = 'http://localhost:8080/eas-media-data/archive';
+  // updateSiteUrl: string = 'http://localhost:8080/standard-site';
+  // updateRoleUrl: string = 'http://localhost:8080/standard-role';
+  // updateChannelUrl: string = 'http://localhost:8080/eas-media-data';
+  // getClassificationUrl: string = 'http://localhost:8080/reference-data?type=clsf';
+  // addClassificationUrl: string = 'http://localhost:8080/standard-classification';
+  // updateClassificationUrl: string = 'http://localhost:8080/standard-classification';
+  // dashboardInfoUrl: string = 'http://localhost:8080/dashboard-info';
+  // getChannelData: string = 'http://localhost:8080/eas-media-data-activity?channelName=';
+  // urlByChannelName: string = 'http://localhost:8080//fetch-audio'
+  getChannelListUrl: string = this.baseUrl + '/eas-media-data';
+  getSiteIdListUrl: string = this.baseUrl + '/reference-data?type=site';
+  getRoleIdListUrl: string = this.baseUrl + '/reference-data?type=role';
+  addChannelUrl: string = this.baseUrl + '/eas-media-data';
+  addSiteUrl: string = this.baseUrl + '/standard-site';
+  addRoleUrl: string = this.baseUrl + '/standard-role';
+  historyDataUrl: string = this.baseUrl + '/eas-media-data-history';
+  archiveChannelsUrl: string = this.baseUrl + '/eas-media-data/archive';
+  updateSiteUrl: string = this.baseUrl + '/standard-site';
+  updateRoleUrl: string = this.baseUrl + '/standard-role';
+  updateChannelUrl: string = this.baseUrl + '/eas-media-data';
+  getClassificationUrl: string = this.baseUrl + '/reference-data?type=clsf';
+  addClassificationUrl: string = this.baseUrl + '/standard-classification';
+  updateClassificationUrl: string = this.baseUrl + 'standard-classification';
+  dashboardInfoUrl: string = this.baseUrl + '/dashboard-info';
+  getChannelData: string = this.baseUrl + '/eas-media-data-activity?channelName=';
+  urlByChannelName: string = this.baseUrl + '/fetch-audio'
 
   constructor(private http: HttpClient) { }
 
@@ -116,14 +135,22 @@ export class DataService {
   };
 
   postFile(fileToUpload: any): Observable<boolean> {
-    const endpoint = 'http://localhost:8080/mass-upload';
+    //const endpoint = 'http://localhost:8080/mass-upload';
+    const endpoint = this.getUrlBase() + '/mass-upload';
     // const formData: FormData = new FormData();
     // formData.append('fileKey', fileToUpload, fileToUpload.name);
     return this.http
       .post(endpoint, fileToUpload)
       // .map(() => { return true; });
       .pipe(map(() => { return true; }));
+  }
 
+  getUrlBase() {
+    if (this.location.port === "4201") {
+      return "http://localhost:8080"
+    } else {
+      return this.location.protocol;
+    }
   }
 
 }
